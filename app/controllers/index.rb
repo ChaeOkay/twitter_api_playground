6e7855493eb8cf3a432593708930b1c2cfbd1015
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-19 12:08:21 -0700

    show user tweets

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,7 +4,7 @@ end
 
 
 get '/:username' do
-  @user = TwitterUser.find_by_username(params[:username])
-
-  @tweets = @user.tweets.first(10)
+  @user = TwitterUser.find_by_screen_name(params[:username])
+  @tweets = @user.tweets
+  erb :user
 end
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,12 +1,27 @@
-class TwitterUser 
-  def self.find_by_username(username)
-    Client.user(username)
+class TwitterUser
+
+  def self.find_by_screen_name(screen_name)
+    self.new(screen_name)
   end
 
-  def tweets
-    @tweets ||= fetch_tweets!
+  attr_reader :screen_name
+  def initialize(screen_name)
+    @screen_name = screen_name
   end
 
-  def fetch_tweets!
+  def user
+    begin
+      self.new(Client.user(@screen_name))
+    rescue Twitter::Error::NotFound
+      return nil
+    end
+  end
+
+  def tweets(count = 10)
+    fetch_tweets!(count)
+  end
+
+  def fetch_tweets!(count)
+    Client.user_timeline(@screen_name, {count: count}).map(&:text)
   end
 end
